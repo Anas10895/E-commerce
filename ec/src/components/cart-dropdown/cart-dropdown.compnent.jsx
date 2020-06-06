b154a16c95e1d0eca0d5fafd56289be247fb856a
AnasShar <anas10895@gmail.com>
2020-06-06 10:31:26 +0300

    updating our app with reselect , replaced cart selector whith memoized selector

--- ec/src/components/cart-dropdown/cart-dropdown.compnent.jsx
+++ ec/src/components/cart-dropdown/cart-dropdown.compnent.jsx
@@ -3,6 +3,7 @@ import CustomButton from '../custom-button/custom-button.component'
 import {connect} from 'react-redux'
 import './cart-dropdown.styles.scss'
 import CartItem from '../cart-item/cart-item.component'
+import {selectCartItems} from '../../redux/cart/cart.selector'
 
 
 const CartDropdown =  ({cartItems}) => {
@@ -18,7 +19,7 @@ const CartDropdown =  ({cartItems}) => {
 
     </div>
 }
-const mapStateToProps = ({cart : {cartItems}})=>({
-    cartItems
+const mapStateToProps = (state)=>({
+    cartItems : selectCartItems(state)
 })
 export default connect(mapStateToProps)(CartDropdown)
--- ec/src/components/cart-icon/cart-icon.component.jsx
+++ ec/src/components/cart-icon/cart-icon.component.jsx
@@ -3,7 +3,7 @@ import {connect} from 'react-redux'
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
 import {ToggleCartState} from '../../redux/cart/cart.actions'
 import './cart-icon.styles.scss'
-
+import {selectCartItemsCount} from '../../redux/cart/cart.selector'
 const CartIcon = ({ToggleCartState , hiddin , itemCount}) => (
     
     <div className="cart-icon" onClick={ToggleCartState}>
@@ -14,10 +14,8 @@ const CartIcon = ({ToggleCartState , hiddin , itemCount}) => (
     
 )
 
-const mapStateToProps = ({cart : {cartItems}})=>({
-    itemCount: cartItems.reduce(
-        (accumaltedQuantity , cartItem) => accumaltedQuantity + cartItem.quantity,0
-    )
+const mapStateToProps = (state)=>({
+    itemCount: selectCartItemsCount(state)
 })
 
 const mapDispatchToProps = dispatch => ({
